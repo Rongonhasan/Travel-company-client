e8dcfbfd08459f88ae1c2574fdd51265fc17de52
rongon <rongonhasan2@gmail.com>
2022-11-09 22:48:00 -0800

    login

--- src/Pages/Home/Blog/Blog.jsx
+++ src/Pages/Home/Blog/Blog.jsx
@@ -37,7 +37,7 @@ const Blog = () => {
         className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
       >
         <div className="collapse-title text-xl font-medium">
-          What is JWT, and how does it work?
+          {/* What is JWT, and how does it work? */}
         </div>
         <div className="collapse-content">
           <p>
